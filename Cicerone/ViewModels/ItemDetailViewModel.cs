8ed478418146a9770ae75e869cc8e3e7670584c3
Steve Bilogan <steve.bilogan@gmail.com>
2019-06-29 09:37:46 -0400

    Initial commit

---
+++ Cicerone/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Cicerone.Models;
+
+namespace Cicerone.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
